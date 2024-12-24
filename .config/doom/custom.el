e9d041030eb0e509f420c70672d0e7598b00e7df
ogisan-7 <ogisanpersonal@gmail.com>
2024-12-24 23:40:09 +0100

    silly

---
+++ .config/doom/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/org/roam/20241214053435-todo.org"))
+ '(package-selected-packages '(org-alert)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
